2e1423b77fc0a7b85009da23abd867fa591d3d06
ShashankJaitly <shashank16vasu@gmail.com>
2019-09-29 12:09:31 +0530

    applied ManytoMany relationship b/w company and job

--- app1/models.py
+++ app1/models.py
@@ -1,18 +1,21 @@
-from python import uuid
 from django.db import models
+from django import validators
 
 # models here !
-class job(models.Model):
-	company = models.CharField(max_legth='30', default=uuid.uuid4(), primary_key=True)
-	profile = models.CharField()
-	founded = models.CharField(blank=True)
-	location = models.CharField(max_legth='100')
+class company(models.Model):
+	name = models.CharField(max_length='30')
+	website = models.UrlField(blank=True)
+	link = models.UrlField()
+	
+class job(model.Model):
+	profile=models.CharField()
+	companies=models.ManytoManyField(company)
+	founded = models.DateField()
+	location = models.CharField(max_length='100')
 	emp_count = models.IntegerField()
-	description = models.TextField(max_legth='300')
-	job_CATEGORY = (('F-O','full_time_office')('P-O','part_time_office')('P-H','part_time_home')('I-O','intern_office')('I-H','intern_home'))
-	job_category = models.CharField(choice=job_CATEGORY)
-    # tags: ["javascript", "angular", "jquery", "nodejs", "reactjs", "mongodb", "vuejs", "sql"]
-    website = models.CharField(validators=[validate_url])
-    job_info = models.TextField(blank=True)
-    link = models.CharField(validators=[validate_url])
-    experience = models.IntegerField(blank=True)
+	description = models.TextField(max_length='300')
+	job_category = models.TextField()
+	tags = models.TextField()
+	job_info = models.TextField(blank=True)
+	experience = models.IntegerField(blank=True)
+
